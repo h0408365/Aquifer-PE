ee004a8026b423d151bf5c00d8a9fd16b49c6168
Harold Stephen Tran <htran1993@outlook.com>
2022-07-14 18:10:03 -0700

    Add files via upload

---
+++ ListingVerificationAddRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.ListingVerifications
+{
+    public class ListingVerificationAddRequest
+    {
+        [Required]
+        public int ListingId { get; set; }
+        [Required]
+        [StringLength(500, MinimumLength = 2)]
+        public string WiFiDocumentUrl { get; set; }
+        [Required]
+        [StringLength(500, MinimumLength = 2)]
+        public string InsuranceDocumentUrl { get; set; }
+        [Required]
+        [StringLength(500, MinimumLength = 2)]
+        public string LocationDocumentUrl { get; set; }
+        public string ApprovedBy { get; set; }
+        public string CreatedBy { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ ListingVerificationData.cs
@@ -0,0 +1,34 @@
+﻿using Sabio.Models.Domain.Listings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.ListingVerification
+{
+    public class ListingVerificationData
+    {
+		public int Id { get; set; }
+		public string InternalName { get; set; }
+		public string Title { get; set; }
+		public string ShortDescription { get; set; }
+		public string Description { get; set; }
+		public short BedRooms { get; set; }
+		public float Baths { get; set; }
+		public HousingType HousingType { get; set; }
+		public  AccessType AccessType { get; set; }
+		public short GuestCapacity { get; set; }
+		public int CostPerNight { get; set; }
+		public int CostPerWeek { get; set; }
+		public TimeSpan CheckInTime { get; set; }
+		public TimeSpan CheckOutTime { get; set; }
+		public int DaysAvailable { get; set; }
+		public int LocationId { get; set; }
+		public Boolean HasVerifiedOwnership { get; set; }
+		public Boolean IsActive { get; set; }
+		public int CreatedBy { get; set; }
+		public DateTime DateCreated { get; set; }
+		public DateTime DateModified { get; set; }
+	}
+}
