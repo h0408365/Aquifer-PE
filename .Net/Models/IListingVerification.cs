963a0bb103b6090105cdbb5626d0449d5455834c
Harold Stephen Tran <htran1993@outlook.com>
2022-07-17 13:57:14 -0700

    Add files via upload

---
+++ .Net/Models/IListingVerification.cs
@@ -0,0 +1,20 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain.ListingVerification;
+using Sabio.Models.Requests.ListingVerifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IListingVerificationService
+    {
+        int Insert(ListingVerificationAddRequest model);
+        void Update(ListingVerificationUpdateRequest model, int id);
+        Paged<ListingVerification> GetPaginate(int pageIndex, int pageSize);
+        Paged<ListingVerification> GetCreatedBy(int pageIndex, int pageSize, int createdBy);
+        ListingVerification Get(int Id);
+    }
+}
