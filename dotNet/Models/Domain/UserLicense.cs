23e5587e49757b4a766fbc486f6ce3370547300b
Harold Stephen Tran <htran1993@outlook.com>
2022-08-14 15:39:26 -0700

    Update UserLicense.cs

--- dotNet/Models/Domain/UserLicense.cs
+++ dotNet/Models/Domain/UserLicense.cs
@@ -1,4 +1,4 @@
-﻿using Sabio.Models.Domain.Organizations;
+using Sabio.Models.Domain.Organizations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +10,14 @@ namespace Sabio.Models.Domain.LicenseVerification
     public class UserLicense
     {
         public int Id { get; set; }
+        public int LicenseTypesId { get; set; }
         public LookUp LicenseType { get; set; }
-        public UserProfile UserProfile { get; set; }
-        public Location Location { get; set; }
-        public State State { get; set; }
+        public int UserId { get; set; }
+        public UserProfileBase CreatedBy { get; set; }
+        public int LocationsId { get; set; }
+        public int LicenseStateId { get; set; }
         public string Url { get; set; }
         public int DateExpires { get; set; }
-        public UserProfileBase CreatedBy { get; set; }
         public DateTime DateCreated { get; set; }   
         public DateTime DateModified { get; set; }
 
