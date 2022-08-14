078accda8a73200aab8aae22a709653bcc17f35f
Harold Stephen Tran <htran1993@outlook.com>
2022-08-14 15:38:50 -0700

    Update LicenseVerificationService.cs

--- dotNet/Services/LicenseVerificationService.cs
+++ dotNet/Services/LicenseVerificationService.cs
@@ -29,54 +29,6 @@ namespace Sabio.Services.LicenseVerification
             
         }
 
-        #region - SelectbyUnexpiredLicenseType -
-        public List<UserLicense> SelectbyUnexpiredLicenseType(int LicenseTypeId, int DateExpires)
-        {
-            string procName = "[dbo].[LicenseVerification_GetBy_UnexpiredLicenseType]";
-            List<UserLicense> list = null;
-            _data.ExecuteCmd(procName,
-                delegate (SqlParameterCollection inputParams)
-                {
-                    inputParams.AddWithValue("@LicenseTypeId", LicenseTypeId);
-                    inputParams.AddWithValue("@DateExpires", DateExpires);
-                },
-                singleRecordMapper: delegate (IDataReader reader, short set)
-                {
-                    int startingIndex = 0;
-                    UserLicense aUserLicense = MapUserLicenseData(reader, ref startingIndex);
-
-                    if (list == null)//if list null make new list and add everything mapping in here
-                    {
-                        list = new List<UserLicense>();//null check
-                    }
-                    list.Add(aUserLicense);
-                });
-            return list;
-        }
-        #endregion
-
-        #region - Create -
-        public int Create(LicenseVerificationAddRequest model)
-        {
-            int id = 0;
-            string procName = "[dbo].[LicenseVerification_Create]";
-            _data.ExecuteNonQuery(procName,//nonQuery sends data to database
-                delegate (SqlParameterCollection col)
-                {
-                    AddCommonParams(model, col); //my mapper
-                    SqlParameter idOut = new SqlParameter("@Id", SqlDbType.Int); //my output which is id
-                    idOut.Direction = ParameterDirection.Output;//this line and hte one above defines the id out
-                    col.Add(idOut);//this adds it in
-                },
-                returnParameters: delegate (SqlParameterCollection returnCollection)//parm coming out which is the id
-                {
-                    object oId = returnCollection["@Id"].Value;//access the id which is an oject
-                    int.TryParse(oId.ToString(), out id); //turns the object into an int.
-                });
-            return id;
-        }
-        #endregion
-
         #region - SelectbyUserLicenseId -
         public UserLicense SelectbyUserLicenseId(int Id)
         {
@@ -121,20 +73,7 @@ namespace Sabio.Services.LicenseVerification
             userLicense.UserId = reader.GetInt32(startingIndex++);
             userLicense.CreatedBy = _profileMapper.Map(reader, ref startingIndex);
             userLicense.LocationsId = reader.GetInt32(startingIndex++);
-            userLicense.Location = new Location();
-            userLicense.Location.Id = reader.GetInt32(startingIndex++);
-            userLicense.Location.LocationType = _lookUp.MapSingleLookUp(reader, ref startingIndex);
-            userLicense.Location.LineOne = reader.GetString(startingIndex++);
-            userLicense.Location.LineTwo = reader.GetString(startingIndex++);
-            userLicense.Location.City = reader.GetString(startingIndex++);
-            userLicense.Location.Zip = reader.GetString(startingIndex++);           
-            userLicense.Location.Latitude = reader.GetSafeDouble(startingIndex++);
-            userLicense.Location.Longitude = reader.GetSafeDouble(startingIndex++);
-            userLicense.LicenseStateId = reader.GetInt32(startingIndex++);
-            userLicense.State = new State();
-            userLicense.State.Id = reader.GetInt32(startingIndex++);    
-            userLicense.State.Code = reader.GetString(startingIndex++);
-            userLicense.State.Name = reader.GetString(startingIndex++);
+            userLicense.LicenseStateId = reader.GetInt32(startingIndex++);  
             userLicense.Url = reader.GetString(startingIndex++);
             userLicense.DateExpires = reader.GetInt32(startingIndex++);
             userLicense.DateCreated = reader.GetDateTime(startingIndex++);
